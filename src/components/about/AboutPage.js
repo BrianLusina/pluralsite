3d63a0b2b761edb44fd21c65dd682166d8f23c07
Brian Lusina <lusinabrian@gmail.com>
2017-05-30 22:09:57 +0300

    Create about and home page
    initial component setup

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,17 @@
+/**
+ * @author lusinabrian on 30/05/17.
+ * @notes: About page
+ */
+
+import React, { Component } from 'react';
+
+export default class About extends Component{
+  render(){
+    return(
+      <div>
+        <h1>About</h1>
+        <p>About PluralVision</p>
+      </div>
+    );
+  }
+}
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,21 @@
+/**
+ * @author lusinabrian on 30/05/17.
+ * @notes: Home page component
+ *
+ */
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+
+export default class HomePage extends Component{
+  render(){
+    return(
+      <div className="jumbotron">
+        <h1>PluralVision</h1>
+        <p>PluralVision Demo</p>
+        <Link to="about" className="btn btn-primary btn-lg"
+              onlyActiveOnIndex="onlyActiveOnIndex">Learn More</Link>
+      </div>
+    );
+  }
+}
