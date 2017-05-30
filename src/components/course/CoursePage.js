2145a2f693c207c4e02785dfd6e5e24d18f4ddc1
Brian Lusina <lusinabrian@gmail.com>
2017-05-30 22:43:19 +0300

    course page creation

---
+++ src/components/course/CoursePage.js
@@ -0,0 +1,20 @@
+/**
+ * @author lusinabrian on 30/05/17.
+ * @notes: Course page
+ */
+
+import React, { Component, PropTypes} from 'react';
+
+export default class CoursePage extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+CoursePage.propTypes = {
+
+};
