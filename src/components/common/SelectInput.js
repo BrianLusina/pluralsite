c7b010b593418ba77c2ebcb09ed331a7d5a42d75
Brian Lusina <lusinabrian@gmail.com>
2017-06-04 14:58:14 +0300

    text input stateles component

--- src/components/common/SelectInput.js
+++ src/components/common/SelectInput.js
@@ -4,18 +4,24 @@ import PropTypes from 'prop-types';
 /**
  * SelectInput stateless component
  */
-const SelectInput = ({myProps}) => {
+const SelectInput = ({ name, label, value, defaultOption, options, onChange, error }) => {
     return (
         <div>
         </div>
     );
-}
+};
 
 /**
  * Prop validation
  */
 SelectInput.propTypes = {
-    myProps: PropTypes.string.isRequired
-}
+  name:PropTypes.string.isRequired,
+  label:PropTypes.string.isRequired,
+  value:PropTypes.number.isRequired,
+  defaultOption:PropTypes.string.isRequired,
+  options:PropTypes.string.isRequired,
+  onChange:PropTypes.func.isRequired,
+  error:PropTypes.string.isRequired
+};
 
-export default SelectInput
+export default SelectInput;
--- src/components/common/TextInput.js
+++ src/components/common/TextInput.js
@@ -4,18 +4,40 @@ import PropTypes from 'prop-types';
 /**
  * TextInput stateless component
  */
-const TextInput = ({myProps}) => {
-    return (
-        <div>
-        </div>
-    );
+const TextInput = ({ name, label, placeholder, value, onChange, error }) => {
+  let wrapperClass = "form-group";
+  if(error && error.length > 0){
+    wrapperClass += " " + "has-error";
+  }
+
+  return (
+    <div className={wrapperClass}>
+      <label htmlFor={name}>{label}</label>
+      <div className="field">
+        <input
+          type="text"
+          name={name}
+          className="form-control"
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+        {error && <div className="alert alert-danger">{error}</div>}
+      </div>
+    </div>
+  );
 };
 
 /**
  * Prop validation
  */
 TextInput.propTypes = {
-    myProps: PropTypes.string.isRequired
+  name:PropTypes.string.isRequired,
+  label:PropTypes.string.isRequired,
+  value:PropTypes.string,
+  placeholder: PropTypes.string,
+  onChange:PropTypes.func.isRequired,
+  error:PropTypes.string
 };
 
 export default TextInput;
