2dd0562ff96cbaf150ff16c1b2522b527fff2c67
Brian Lusina <lusinabrian@gmail.com>
2017-06-04 10:58:37 +0300

    connect courses page to store

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -4,8 +4,11 @@
  */
 
 import React, { Component, PropTypes} from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../../actions/courseActions';
 
-export default class CoursePage extends Component{
+
+class CoursePage extends Component{
   constructor(props, context){
     super(props, context);
 
@@ -35,13 +38,22 @@ export default class CoursePage extends Component{
    * @param {object} event*/
   onClickSave(event){
     event.preventDefault();
-    alert(`saving title ${this.state.course.title}`);
+    this.props.dispatch(courseActions.createCourse(this.state.course));
+  }
+
+  courseRow(course, index){
+    return (
+      <div key={index}>
+        {course.title}
+      </div>
+    );
   }
 
   render(){
     return(
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add course</h2>
         <input
           type="text"
@@ -60,5 +72,30 @@ export default class CoursePage extends Component{
 }
 
 CoursePage.propTypes = {
-
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
 };
+
+/**
+ * Maps state of props in this component to store of component
+ * @param ownProps
+ * @param state within redux store
+ * @return {Object}
+ * */
+function mapStateToProps(state, ownProps){
+  return {
+    courses: state.courses
+  };
+}
+
+/**
+ * Fires actions to redux store
+ *
+ * */
+// function mapDispatchToProps(){
+//
+// }
+
+/**
+ * Connect*/
+export default connect(mapStateToProps /*,mapDispatchToProps*/)(CoursePage);
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -13,7 +13,6 @@ import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 export default function configureStore(initialState){
   return createStore(
     rootReducer, initialState,
-    applyMiddleware(reduxImmutableStateInvariant(),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-  );
+    applyMiddleware(reduxImmutableStateInvariant())
+  )
 }
