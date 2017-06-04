986d86f8ccaa126f39bfc6c0a3d88dbfa39e0827
Brian Lusina <lusinabrian@gmail.com>
2017-06-04 15:18:22 +0300

    create author actions

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,37 +2,43 @@ import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
-
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends Component {
-    constructor(props, context) {
-        super(props, context);
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      course: Object.assign({}, props.course),
+      errors: {}
+    };
+  }
 
-    }
-
-    render() {
-        return (
-            <div>
-              <h1>Manage Course</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <CourseForm
+        allAuthors={[]}
+        course={this.state.course} onSave="" onChange=""
+        errors={this.state.errors}
+      />
+    );
+  }
 }
 
 ManageCoursePage.propTypes = {
-
+  course: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
-    return {
-        state: state
-    };
+
+  return {
+    course: state
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        actions: bindActionCreators(courseActions, dispatch)
-    };
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
