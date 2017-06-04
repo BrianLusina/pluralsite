6ffb89c9da16a7fbedef48930094415f34e1d8b0
Brian Lusina <lusinabrian@gmail.com>
2017-06-04 13:33:38 +0300

    course list row and course list creation

--- src/components/course/CourseList.js
+++ src/components/course/CourseList.js
@@ -1,4 +1,33 @@
 /**
  * @author lusinabrian on 04/06/17.
- * @notes:
+ * @notes: Course list component used to render a list of courses
  */
+import React, { PropTypes } from 'react';
+import CourseListRow from './CourseListRow';
+
+
+const CourseList = ({courses, deleteCourse}) => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>&nbsp;</th>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Category</th>
+          <th>Length</th>
+        </tr>
+      </thead>
+      <tbody>
+      {courses.map(course => <CourseListRow key={course.id} course={course}/>)}
+      </tbody>
+    </table>
+  );
+};
+
+CourseList.propTypes = {
+  courses: PropTypes.array.isRequired,
+  deleteCourse: PropTypes.func
+};
+
+export default CourseList;
--- src/components/course/CourseListRow.js
+++ src/components/course/CourseListRow.js
@@ -1,4 +1,26 @@
 /**
  * @author lusinabrian on 04/06/17.
- * @notes:
+ * @notes: a single course list row
  */
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+
+
+const CourseListRow = ({course}) => {
+  return(
+    <tr>
+      <td><a href={course.watchHref} target="_blank">Watch</a></td>
+      <td><Link to={'/course/' + course.id}>{course.title}</Link></td>
+      <td>{course.authorId}</td>
+      <td>{course.category}</td>
+      <td>{course.length}</td>
+    </tr>
+  );
+};
+
+CourseListRow.propTypes = {
+  key: PropTypes.number.isRequired,
+  course: PropTypes.object.isRequired,
+};
+
+export default CourseListRow;
