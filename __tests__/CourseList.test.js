4aa948e4b4747ebe5ea6987ee0b7166de51cacba
Brian Lusina <lusinabrian@gmail.com>
2017-06-06 23:07:50 +0300

    create course list test

---
+++ __tests__/CourseList.test.js
@@ -0,0 +1,22 @@
+import expect from 'expect';
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import CourseList from '../src/components/course/CourseList';
+
+function setup() {
+    const props = {
+      courses: [],
+      deleteCourse: () => {}
+    };
+
+    return shallow(<CourseList {...props}/>);
+}
+
+describe("CourseList should", () => {
+
+    it("render without crashing", () => {
+        const wrapper = setup();
+        expect(wrapper.find("table").length).toBe(1);
+    });
+
+});
