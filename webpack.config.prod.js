aa8d5a65f0ccfeb060768d8e676caa93d7796bbf
Brian Lusina <lusinabrian@gmail.com>
2017-06-08 22:04:16 +0300

    fix production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -29,9 +29,9 @@ export default {
     new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
-    loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /(\.css)$/, loaders: ExtractTextPlugin.extract('css?sourceMap')},
+    loaders:[
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loader: 'babel'},
+      {test: /(\.css)$/, loader: ExtractTextPlugin.extract('css?sourceMap')},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
